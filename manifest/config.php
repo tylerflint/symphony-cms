13c33de8ad36e941353928949443a2a06dfa4ca6
tylerflint <tylerflint@gmail.com>
2011-06-24 08:48:34 -0600

    symphony-cms installer ran

---
+++ manifest/config.php
@@ -0,0 +1,96 @@
+<?php
+
+	$settings = array(
+
+
+		###### ADMIN ######
+		'admin' => array(
+			'max_upload_size' => '5242880',
+		),
+		########
+
+
+		###### SYMPHONY ######
+		'symphony' => array(
+			'pagination_maximum_rows' => '17',
+			'allow_page_subscription' => '1',
+			'lang' => 'en',
+			'version' => '2.2.1',
+			'pages_table_nest_children' => 'yes',
+			'strict_error_handling' => 'yes',
+			'cookie_prefix' => 'sym-',
+		),
+		########
+
+
+		###### LOG ######
+		'log' => array(
+			'archive' => '1',
+			'maxsize' => '102400',
+		),
+		########
+
+
+		###### GENERAL ######
+		'general' => array(
+			'sitename' => 'Symphony CMS',
+			'useragent' => 'Symphony/2.2.1',
+		),
+		########
+
+
+		###### IMAGE ######
+		'image' => array(
+			'cache' => '1',
+			'quality' => '90',
+		),
+		########
+
+
+		###### DATABASE ######
+		'database' => array(
+			'character_set' => 'utf8',
+			'character_encoding' => 'utf8',
+			'runtime_character_set_alter' => '1',
+			'query_caching' => 'default',
+			'host' => 'localhost',
+			'port' => '3306',
+			'user' => 'root',
+			'password' => 'root',
+			'db' => 'symphony-cms',
+			'tbl_prefix' => 'sym_',
+		),
+		########
+
+
+		###### PUBLIC ######
+		'public' => array(
+			'display_event_xml_in_source' => 'yes',
+		),
+		########
+
+
+		###### REGION ######
+		'region' => array(
+			'time_format' => 'H:i',
+			'date_format' => 'd F Y',
+			'datetime_separator' => ' ',
+			'timezone' => 'America/Denver',
+		),
+		########
+
+
+		###### FILE ######
+		'file' => array(
+			'write_mode' => '0775',
+		),
+		########
+
+
+		###### DIRECTORY ######
+		'directory' => array(
+			'write_mode' => '0775',
+		),
+		########
+	);
+
